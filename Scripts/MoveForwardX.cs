80feb94700e631ccf89e5d99d2e5a217af81bf13
MudassirHabib950 <118183064+MudassirHabib950@users.noreply.github.com>
2022-12-06 11:09:11 +0500

    Initial commit

---
+++ Scripts/MoveForwardX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForwardX : MonoBehaviour
+{
+   public float xRange = 10.0f;
+
+    // Update is called once per frame
+    void Update() 
+    {
+        // Moving Player Forward With Speed  in Z direction by Pressing W key 
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0,0,15 * Time.deltaTime);
+        }
+         // Moving Player Forward With Speed  in -Z direction by Pressing S key 
+         if(Input.GetKey(KeyCode.S))
+         {
+        transform.Translate(0,0,-15 * Time.deltaTime );
+          }
+          }
+}
---
+++ Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+   public float speed = 10.0f;
+   public float cooldown = 1.0f;
+   public GameObject ballPrefab;
+   void Start()
+   {
+
+   } 
+ void Update()
+ {
+    // On Space Bar key Press send fire
+    if(Input.GetKey(KeyCode.Space) && cooldown <= 0)
+    {
+        Instantiate(ballPrefab,transform.position,ballPrefab.transform.rotation);
+        cooldown = 1.0f;
+    }
+    if(cooldown > 0)
+    {
+       cooldown -= Time.deltaTime;
+
+    }
+ 
+ }
+}
